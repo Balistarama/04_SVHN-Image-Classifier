f198d0da333162ab68da56853c6f5e901dce291c
Balistarama <Balistarama@users.noreply.github.com>
2016-12-30 22:14:32 +1100

    Add files via upload

--- data/data_processor.py
+++ data/data_processor.py
@@ -9,157 +9,42 @@ from scipy import ndimage
 from sklearn.linear_model import LogisticRegression
 from six.moves.urllib.request import urlretrieve
 from six.moves import cPickle as pickle
+from six.moves import urllib
+from scipy.io import loadmat
+import h5py
 
-"""
-Now let's load the data in a more manageable format. Since, depending on your computer 
-setup you might not be able to fit it all in memory, we'll load each class into a 
-separate dataset, store them on disk and curate them independently. Later we'll 
-merge them into a single dataset of manageable size.
 
-We'll convert the entire dataset into a 3D array (image index, x, y) of floating point 
-values, normalized to have approximately zero mean and standard deviation ~0.5 to 
-make training easier down the road. A few images might not be readable, we'll just skip them.
-"""
+train_f = loadmat("raw/Format 2/train_32x32.mat")
+extra_f = loadmat("raw/Format 2/extra_32x32.mat")
+test_f = loadmat("raw/Format 2/test_32x32.mat")
 
-image_size = 28  # Pixel width and height.
-pixel_depth = 255.0  # Number of levels per pixel.
+trainx = train_f['X']					# rows of the images (32x32x3)
+trainy = train_f['y'].flatten()				# rows of the label values
+extrax = extra_f['X']					# rows of the images (32x32x3)
+extray = extra_f['y'].flatten()				# rows of the label values
+train_dataset = np.concatenate((trainx[...,np.newaxis], extrax[...,np.newaxis]), axis=3)
+train_labels  = np.concatenate((trainy, extray))
+print('train_dataset shape: ' + str(train_dataset.shape))
+print('train_labels shape: ' + str(train_labels.shape))
 
-def load_letter(folder, min_num_images):
-  """Load the data for a single letter label."""
-  image_files = os.listdir(folder)
-  dataset = np.ndarray(shape=(len(image_files), image_size, image_size), dtype=np.float32)
-  print(folder)
-  num_images = 0
-  for image in image_files:
-    image_file = os.path.join(folder, image)
-    try:
-      image_data = (ndimage.imread(image_file).astype(float) - pixel_depth / 2) / pixel_depth	# Converts 0-255 range to -1 to +1
-      if image_data.shape != (image_size, image_size):
-        raise Exception('Unexpected image shape: %s' % str(image_data.shape))
-      dataset[num_images, :, :] = image_data
-      num_images = num_images + 1
-    except IOError as e:
-      print('Could not read:', image_file, ':', e, '- it\'s ok, skipping.')
-    
-  dataset = dataset[0:num_images, :, :]
-  if num_images < min_num_images:
-    raise Exception('Many fewer images than expected: %d < %d' %
-                    (num_images, min_num_images))
-    
-  print('Full dataset tensor:', dataset.shape)
-  print('Mean:', np.mean(dataset))
-  print('Standard deviation:', np.std(dataset))
-  return dataset
-        
-def maybe_pickle(data_folders, min_num_images_per_class, force=False):
-  dataset_names = []
-  for folder in data_folders:
-    set_filename = folder + '.pickle'
-    dataset_names.append(set_filename)
-    if os.path.exists(set_filename) and not force:
-      # You may override by setting force=True.
-      print('%s already present - Skipping pickling.' % set_filename)
-    else:
-      print('Pickling %s.' % set_filename)
-      dataset = load_letter(folder, min_num_images_per_class)
-      try:
-        with open(set_filename, 'wb') as f:
-          pickle.dump(dataset, f, pickle.HIGHEST_PROTOCOL)
-      except Exception as e:
-        print('Unable to save data to', set_filename, ':', e)
-  
-  return dataset_names
+test_dataset = test_f['X']					# rows of the images (32x32x3)
+test_labels = test_f['y'].flatten()				# rows of the label values
+print('test_dataset shape: ' + str(test_dataset.shape))
+print('test_labels shape: ' + str(test_labels.shape))
 
-train_datasets = maybe_pickle("raw/train", 45000)
-test_datasets = maybe_pickle("raw/test", 1800)
-
-"""
-Merge and prune the training data as needed. Depending on your computer setup, you might 
-not be able to fit it all in memory, and you can tune train_size as needed. 
-The labels will be stored into a separate array of integers 0 through 9.
-
-Also create a validation dataset for hyperparameter tuning.
-"""
-
-def make_arrays(nb_rows, img_size):
-  if nb_rows:
-    dataset = np.ndarray((nb_rows, img_size, img_size), dtype=np.float32)
-    labels = np.ndarray(nb_rows, dtype=np.int32)
-  else:
-    dataset, labels = None, None
-  return dataset, labels
-
-def merge_datasets(pickle_files, train_size, valid_size=0):
-  num_classes = len(pickle_files)
-  valid_dataset, valid_labels = make_arrays(valid_size, image_size)
-  train_dataset, train_labels = make_arrays(train_size, image_size)
-  vsize_per_class = valid_size // num_classes
-  tsize_per_class = train_size // num_classes
-    
-  start_v, start_t = 0, 0
-  end_v, end_t = vsize_per_class, tsize_per_class
-  end_l = vsize_per_class+tsize_per_class
-  for label, pickle_file in enumerate(pickle_files):       
-    try:
-      with open(pickle_file, 'rb') as f:
-        letter_set = pickle.load(f)
-        # let's shuffle the letters to have random validation and training set
-        np.random.shuffle(letter_set)
-        if valid_dataset is not None:
-          valid_letter = letter_set[:vsize_per_class, :, :]
-          valid_dataset[start_v:end_v, :, :] = valid_letter
-          valid_labels[start_v:end_v] = label
-          start_v += vsize_per_class
-          end_v += vsize_per_class
-                    
-        train_letter = letter_set[vsize_per_class:end_l, :, :]
-        train_dataset[start_t:end_t, :, :] = train_letter
-        train_labels[start_t:end_t] = label
-        start_t += tsize_per_class
-        end_t += tsize_per_class
-    except Exception as e:
-      print('Unable to process data from', pickle_file, ':', e)
-      raise
-    
-  return valid_dataset, valid_labels, train_dataset, train_labels
-            
-            
-train_size = 200000
-valid_size = 10000
-test_size = 10000
-
-valid_dataset, valid_labels, train_dataset, train_labels = merge_datasets(train_datasets, train_size, valid_size)
-_, _, test_dataset, test_labels = merge_datasets(test_datasets, test_size)
-
-print('Training:', train_dataset.shape, train_labels.shape)
-print('Validation:', valid_dataset.shape, valid_labels.shape)
-print('Testing:', test_dataset.shape, test_labels.shape)
-
-"""
-Next, we'll randomize the data. It's important to have the labels well 
-shuffled for the training and test distributions to match.
-"""
-
-def randomize(dataset, labels):
-  permutation = np.random.permutation(labels.shape[0])
-  shuffled_dataset = dataset[permutation,:,:]
-  shuffled_labels = labels[permutation]
-  return shuffled_dataset, shuffled_labels
-train_dataset, train_labels = randomize(train_dataset, train_labels)
-test_dataset, test_labels = randomize(test_dataset, test_labels)
-valid_dataset, valid_labels = randomize(valid_dataset, valid_labels)
+#valid_dataset, valid_labels
 
 """ Finally, let's save the data for later reuse """
-
-pickle_file = 'notMNIST.pickle'
+pickle_file = 'SVHN.pickle'
+print('Doing some pickling....')
 
 try:
   f = open(pickle_file, 'wb')
   save = {
     'train_dataset': train_dataset,
     'train_labels': train_labels,
-    'valid_dataset': valid_dataset,
-    'valid_labels': valid_labels,
+#    'valid_dataset': valid_dataset,
+#    'valid_labels': valid_labels,
     'test_dataset': test_dataset,
     'test_labels': test_labels,
     }
