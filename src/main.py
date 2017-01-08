1ec95dcd42c5ab01e0e88a274fb1b25165dd0a62
Name <Balistarama@users.noreply.github.com>
2017-01-08 17:07:59 +1100

    Add files via upload
    Finally got TensorBoard working!

--- src/main.py
+++ src/main.py
@@ -31,6 +31,18 @@ def max_pool_2x2(x):
   """ strides: The stride of the sliding window for each dimension of the input tensor. """
   return tf.nn.max_pool(value=x, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='SAME')
 
+def variable_summaries(var, name):
+  """Automatically attaches a bunch of summaries to a Tensor for TensorBoard."""
+  with tf.name_scope('Summaries'):
+    mean = tf.reduce_mean(var)
+    tf.summary.scalar('Mean/' + name, mean)
+    with tf.name_scope('stddev'):
+      stddev = tf.sqrt(tf.reduce_mean(tf.square(var - mean)))
+    tf.summary.scalar('stddev/' + name, stddev)
+    tf.summary.scalar('max/' + name, tf.reduce_max(var))
+    tf.summary.scalar('min/' + name, tf.reduce_min(var))
+    tf.summary.histogram(name, var)
+
 # Import the dataset from the main pickle file
 print('\nImporting Data from "' + DATASET + '"')
 
@@ -61,41 +73,67 @@ print('\n')
 sess = tf.InteractiveSession()
 
 """ DEFINE VARIABLES WITH PLACEHOLDERS """
-with tf.name_scope('Input_Images'):
+with tf.name_scope('Inputs'):
     x = tf.placeholder(tf.float32, shape=[None, IMAGE_SIZE, IMAGE_SIZE, NUM_CHANNELS], name="x-input")
-
-with tf.name_scope('Input_Labels'):
     y_ = tf.placeholder(tf.float32, shape=[None, NUM_LABELS], name="y-input")
-
+    tf.summary.image('input', x, 10)
+	
 """ LAYER 1 """
-with tf.name_scope('Layer_1_Weights'):
-    W_conv1 = weight_variable([CONVOLUTION_SIZE, CONVOLUTION_SIZE, NUM_CHANNELS, 28])
+layer_name = 'Layer_1'
+with tf.name_scope(layer_name):
+    with tf.name_scope('Weights'):
+        W_conv1 = weight_variable([CONVOLUTION_SIZE, CONVOLUTION_SIZE, NUM_CHANNELS, 28])
+        variable_summaries(W_conv1, layer_name + '/Weights')
 
-with tf.name_scope('Layer_1_Biases'):    
-    b_conv1 = bias_variable([28])
-
-h_conv1 = tf.nn.relu(conv2d(x, W_conv1) + b_conv1)							# Output Size = 32@32x32
-h_pool1 = max_pool_2x2(h_conv1)										# Output Size = 32@16x16
-
-""" LAYER 2 """
-with tf.name_scope('Layer_2_Weights'):
-    W_conv2 = weight_variable([CONVOLUTION_SIZE, CONVOLUTION_SIZE, 28, 56])
-
-with tf.name_scope('Layer_2_Biases'):
-    b_conv2 = bias_variable([56])
+    with tf.name_scope('Biases'):
+        b_conv1 = bias_variable([28])
+        variable_summaries(b_conv1, layer_name + '/Biases')    
     
-h_conv2 = tf.nn.relu(conv2d(h_pool1, W_conv2) + b_conv2)						# Output Size = 64@16x16
-h_pool2 = max_pool_2x2(h_conv2)										# Output Size = 64@8x8
+    with tf.name_scope('Wx_plus_b'):
+        preactivate = conv2d(x, W_conv1) + b_conv1
+        tf.summary.histogram(layer_name + '/pre_activations', preactivate)
+        activations = tf.nn.relu(preactivate, name='activation')
+        tf.summary.histogram(layer_name + '/activations', activations)  
+        h_pool1 = max_pool_2x2(activations)
+        tf.summary.histogram(layer_name + '/poolings', h_pool1)  
+        
+""" LAYER 2 """
+layer_name = 'Layer_2'
+with tf.name_scope(layer_name):
+    with tf.name_scope('Weights'):
+        W_conv2 = weight_variable([CONVOLUTION_SIZE, CONVOLUTION_SIZE, 28, 56])
+        variable_summaries(W_conv2, layer_name + '/Weights')
+
+    with tf.name_scope('Biases'):
+        b_conv2 = bias_variable([56])
+        variable_summaries(b_conv2, layer_name + '/Biases')    
+    
+    with tf.name_scope('Wx_plus_b'):
+        preactivate = conv2d(h_pool1, W_conv2) + b_conv2
+        tf.summary.histogram(layer_name + '/pre_activations', preactivate)
+        activations = tf.nn.relu(preactivate, name='activation')
+        tf.summary.histogram(layer_name + '/activations', activations)  
+        h_pool2 = max_pool_2x2(activations)
+        tf.summary.histogram(layer_name + '/poolings', h_pool2)  
 
 """ FULLY CONNECTED LAYER """
-with tf.name_scope('FCL_1_Weights'):
-    W_fc1 = weight_variable([8 * 8 * 56, NUM_NEURONS])	
+layer_name = 'FCL_1'
+with tf.name_scope(layer_name):
+    with tf.name_scope('Weights'):
+        W_fc1 = weight_variable([8 * 8 * 56, NUM_NEURONS])	
+        variable_summaries(W_fc1, layer_name + '/Weights')
 
-with tf.name_scope('FCL_1_Biases'):						# Must match 64@8x8 Size
-    b_fc1 = bias_variable([NUM_NEURONS])
-
-h_pool2_flat = tf.reshape(h_pool2, [-1, 8 * 8 * 56])
-h_fc1 = tf.nn.relu(tf.matmul(h_pool2_flat, W_fc1) + b_fc1)
+    with tf.name_scope('Biases'):
+        b_fc1 = bias_variable([NUM_NEURONS])
+        variable_summaries(b_fc1, layer_name + '/Biases')    
+    
+    h_pool2_flat = tf.reshape(h_pool2, [-1, 8 * 8 * 56])
+    
+    with tf.name_scope('Wx_plus_b'):
+        preactivate = tf.matmul(h_pool2_flat, W_fc1) + b_fc1
+        tf.summary.histogram(layer_name + '/pre_activations', preactivate)
+        h_fc1 = tf.nn.relu(preactivate, name='activation')
+        tf.summary.histogram(layer_name + '/activations', h_fc1) 
 
 """ DROPOUT LAYER """
 with tf.name_scope('Dropout'):
@@ -103,27 +141,32 @@ with tf.name_scope('Dropout'):
     tf.summary.scalar('dropout_keep_probability', keep_prob)
     h_fc1_drop = tf.nn.dropout(h_fc1, keep_prob)
 
-""" READOUT LAYER """
-with tf.name_scope('FCL_2_Weights'):
-    W_fc2 = weight_variable([NUM_NEURONS, NUM_LABELS])
-    
-with tf.name_scope('FCL_2_Biases'):
-    b_fc2 = bias_variable([NUM_LABELS])
-
 """ OUTPUT """
-with tf.name_scope("Softmax"):
-    y = tf.nn.softmax(tf.matmul(h_fc1_drop, W_fc2) + b_fc2)
+layer_name = 'Output_Layer'
+with tf.name_scope(layer_name):
+    with tf.name_scope('Weights'):
+        W_fc2 = weight_variable([NUM_NEURONS, NUM_LABELS])
+        variable_summaries(W_fc2, layer_name + '/Weights')
+
+    with tf.name_scope('Biases'):
+        b_fc2 = bias_variable([NUM_LABELS])
+        variable_summaries(b_fc2, layer_name + '/Biases')  
+
+    with tf.name_scope('Wx_plus_b'):
+        preactivate = tf.matmul(h_fc1_drop, W_fc2) + b_fc2
+        tf.summary.histogram(layer_name + '/pre_activations', preactivate)
+        y = tf.nn.softmax(preactivate, name='activation')
+        tf.summary.histogram(layer_name + '/activations', y) 
 
 """ LOSS FUNCTION """
 # Define the loss function or cross entropy
 with tf.name_scope('Cross_Entropy'):
-    with tf.name_scope('Total'):
-        cross_entropy = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(y, y_))
+    cross_entropy = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(y, y_))
 tf.summary.scalar("Cross_Entropy", cross_entropy)
 	
 # Define model training
 with tf.name_scope('Train'):
-    train_step = tf.train.AdamOptimizer(1e-4).minimize(cross_entropy)
+    train_step = tf.train.AdamOptimizer(0.001).minimize(cross_entropy)
         
 with tf.name_scope('Accuracy'):
     with tf.name_scope('Correct_Prediction'):
@@ -132,12 +175,12 @@ with tf.name_scope('Accuracy'):
     with tf.name_scope('Accuracy'):
         # Casts the tensor to a "float" and then calculates the mean value
         accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
-tf.summary.scalar("Accuracy", accuracy)    
+    tf.summary.scalar("Accuracy", accuracy)    
 
 # Merge all the summaries and write them out 
 merged = tf.summary.merge_all()
 validation_writer = tf.summary.FileWriter(LOGS_PATH)
-train_writer = tf.summary.FileWriter(LOGS_PATH)
+train_writer = tf.summary.FileWriter(LOGS_PATH, sess.graph)
 
 # Initialise all the variables
 tf.global_variables_initializer().run()
@@ -154,15 +197,17 @@ for i in range(TRAINING_ITERATIONS):
     start_index = (i * BATCH_SIZE) % valid_dataset.shape[0]
     finish_index = start_index + BATCH_SIZE
     valid_batch = [valid_dataset[start_index:finish_index, :], valid_labels[start_index:finish_index, :]]
-    validation_accuracy = accuracy.eval(feed_dict={x: valid_batch[0], y_: valid_batch[1], keep_prob: 1.0})
-    #print('Training Accuracy (Step {:0d}): {:.3f}%'.format(i, 100 * validation_accuracy))
-    #validation_writer.add_summary(validation_accuracy, i)
+    #validation_accuracy = accuracy.eval(feed_dict={x: valid_batch[0], y_: valid_batch[1], keep_prob: 1.0})
+    summary, acc = sess.run([merged, accuracy], feed_dict={x: valid_batch[0], y_: valid_batch[1], keep_prob: 1.0})
+    validation_writer.add_summary(summary, i)
+    print('Training Accuracy (Step %s): %s' % (i, acc))
 
   start_index = (i * BATCH_SIZE) % train_dataset.shape[0]
   finish_index = start_index + BATCH_SIZE
   train_batch = [train_dataset[start_index:finish_index, :], train_labels[start_index:finish_index, :]]    
-  train_accuracy = train_step.run(feed_dict={x: train_batch[0], y_: train_batch[1], keep_prob: 0.5})
-  train_writer.add_summary(train_accuracy, i)
+  #train_accuracy = train_step.run(feed_dict={x: train_batch[0], y_: train_batch[1], keep_prob: 0.5})
+  summary, _ = sess.run([merged, train_step], feed_dict={x: train_batch[0], y_: train_batch[1], keep_prob: 0.5})
+  train_writer.add_summary(summary, i)
 
 # Finally test and print out the results
 final_accuracy = []
@@ -172,6 +217,9 @@ for i in range(int(test_dataset.shape[0]/BATCH_SIZE)):
   finish_index = start_index + BATCH_SIZE
   test_batch = [test_dataset[start_index:finish_index, :], test_labels[start_index:finish_index, :]]
   final_accuracy.append(accuracy.eval(feed_dict={x: test_batch[0], y_: test_batch[1], keep_prob: 1.0}))
+
+validation_writer.close()
+train_writer.close()
 
 print('\n*************************************************')
 print('Final Model Accuracy: {:.2f}% (Runtime: {:.2f} Seconds)\n'.format(100*(sum(final_accuracy) / float(len(final_accuracy))), (time.time() - start_time)) )
